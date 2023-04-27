bfa23ef2daad620b594eddf2e4c44373b1c50543
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-04-27 22:03:59 +0200

    manage xml responces type

--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(opt=>opt.ReturnHttpNotAcceptable=true)
     .AddNewtonsoftJson()
-    .AddXmlDataContractSerializerFormatters();
+   .AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
