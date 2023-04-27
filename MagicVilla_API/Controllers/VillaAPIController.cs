52a8ca4c154a9807eb61e124d7d6f060e33c26de
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-04-27 18:10:26 +0200

    create VillaModel and create first endPoint in VillaAPIController

---
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas() 
+        {
+           return new List<Villa>() 
+           {
+             new Villa {Id = 1,Name = "Pool View"},
+             new Villa {Id = 1,Name = "Beach View"},
+           };
+        }
+    }
+}
