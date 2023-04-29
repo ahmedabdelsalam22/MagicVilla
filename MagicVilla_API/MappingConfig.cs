75aa4a2a243b2ac348813bd5c623e7a46e1c5be1
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-04-29 18:17:19 +0300

    install auto mapper packages and create MappingConfig and register service in program cs with DI

---
+++ MagicVilla_API/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dtos;
+
+namespace MagicVilla_API
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            CreateMap<Villa, VillaDto>();
+            CreateMap<VillaDto, Villa>();
+            CreateMap<Villa, VillaCreateDto>().ReverseMap();
+            CreateMap<Villa, VillaUpdateDto>().ReverseMap();
+        }
+    }
+}
