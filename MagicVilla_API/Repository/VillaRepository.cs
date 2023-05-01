e8a1116a7f523d26cfc359e415e1d09639914e2f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-05-01 21:56:09 +0300

    add Generic Repository

--- MagicVilla_API/Repository/VillaRepository.cs
+++ MagicVilla_API/Repository/VillaRepository.cs
@@ -15,49 +15,6 @@ namespace MagicVilla_API.Repository
         {
             _db = db;
         }
-
-        public async Task CreateAsync(Villa entity)
-        {
-            await _db.Villas.AddAsync(entity);
-            await SaveAsync();
-        }
-
-        public async Task<Villa> GetAsync(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Villa> villas = _db.Villas;
-            if (!tracked) 
-            {
-                villas= villas.AsNoTracking();
-            }
-            if (filter != null)
-            {
-                villas = villas.Where(filter);
-            }
-            return await villas.FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Villa>> GetAllAsync(Expression<Func<Villa, bool>> filter = null)
-        {
-            IQueryable<Villa> villas = _db.Villas;
-
-            if (filter != null) 
-            {
-                villas = villas.Where(filter);
-            }
-            return await villas.ToListAsync();
-        }
-
-        public async Task RemoveAsync(Villa entity)
-        {
-            _db.Villas.Remove(entity);
-            await SaveAsync();
-        }
-
-        public async Task SaveAsync()
-        {
-           await _db.SaveChangesAsync();
-        }
-
         public async Task UpdateAsync(Villa entity)
         {
             _db.Villas.Update(entity);
