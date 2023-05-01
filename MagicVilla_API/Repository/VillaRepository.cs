6a9b7e711fe91203ccb7b7fa03af4dcb6b626492
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-05-01 20:49:54 +0300

    create VillaRepository

---
+++ MagicVilla_API/Repository/VillaRepository.cs
@@ -0,0 +1,61 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MagicVilla_API.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaRepository(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+        public async Task Create(Villa entity)
+        {
+            await _db.Villas.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<Villa> villas = _db.Villas;
+            if (!tracked) 
+            {
+                villas= villas.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                villas = villas.Where(filter);
+            }
+            return await villas.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null)
+        {
+            IQueryable<Villa> villas = _db.Villas;
+
+            if (filter != null) 
+            {
+                villas = villas.Where(filter);
+            }
+            return await villas.ToListAsync();
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _db.Villas.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+           await _db.SaveChangesAsync();
+        }
+    }
+}
