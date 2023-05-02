b38f0c8ffe5b1c3c478f4c6eb8f971260522cd70
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-05-02 20:32:41 +0300

    install autoMapper packages and MappingConfig class in web projects

---
+++ MagicVilla_web/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVilla_web.Models.Dtos;
+
+namespace MagicVilla_web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            CreateMap<VillaDto, VillaCreateDto>().ReverseMap();
+            CreateMap<VillaDto, VillaUpdateDto>().ReverseMap();
+
+            CreateMap<VillaNumberDto, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDto, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
