f2e4b1a9bbcb0dae8da8cb9aa75ada4546dad886
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-05-04 18:38:05 +0300

    IndexVillaNumber action and view

--- MagicVilla_web/Models/Dtos/VillaNumberDto.cs
+++ MagicVilla_web/Models/Dtos/VillaNumberDto.cs
@@ -10,5 +10,6 @@ namespace MagicVilla_web.Models.Dtos
         public string SpecialDetails { get; set; }
         [Required]
         public int VillaId { get; set; }
+        public VillaDto Villa { get; set; }
     }
 }
