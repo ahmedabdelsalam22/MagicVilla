041e4bc54011e0dfc96c39803d6a85580b842abe
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-05-08 18:39:01 +0300

    add Login body in AuthController

--- MagicVilla_web/Controllers/AuthController.cs
+++ MagicVilla_web/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
-﻿using MagicVilla_web.Models;
+﻿using MagicVilla_Utility;
+using MagicVilla_web.Models;
 using MagicVilla_web.Services.IServices;
 using MagicVilla_Web.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MagicVilla_web.Controllers
 {
@@ -25,8 +27,20 @@ namespace MagicVilla_web.Controllers
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> Login(LoginRequestDTO obj)
 		{
-			return View(obj);
-		}
+            APIResponse response = await _authService.LoginAsync<APIResponse>(obj);
+            if (response != null && response.IsSuccess)
+            {
+                LoginResponseDTO model = JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(response.Result));
+
+				HttpContext.Session.SetString(SD.SessionToken,model.Token);
+				return RedirectToAction("Index","Home");
+			}
+            else
+            {
+                ModelState.AddModelError("CustomError", response.ErrorMessages.FirstOrDefault());
+                return View(obj);
+            }
+        }
 
 
 		[HttpGet]
